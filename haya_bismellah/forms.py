0ff8bcb7899eef33c51ee62383868c8964d9b893
Amenallah28 <amenallahmrabet9@gmail.com>
2024-07-10 16:51:30 +0100

    file that defines the ReviewForm class to collect the review data

---
+++ haya_bismellah/forms.py
@@ -0,0 +1,9 @@
+
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
+
+class ReviewForm(FlaskForm):
+    rating = IntegerField('Rating (1-5)', validators=[DataRequired(), NumberRange(min=1, max=5)])
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Submit')
