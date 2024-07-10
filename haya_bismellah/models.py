db7014583fa9b1934a13a3cc5204512b384171c7
Amenallah28 <amenallahmrabet9@gmail.com>
2024-07-10 16:52:01 +0100

    added the Review class

--- haya_bismellah/models.py
+++ haya_bismellah/models.py
@@ -1,8 +1,21 @@
+"""models.py"""
+
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):  
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False) 
-    password = db.Column(db.String(150), nullable=False)  
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
     first_name = db.Column(db.String(150), nullable=False)
+    reviews = db.relationship('Review')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user = db.relationship('User')
