1b3c9c91943826cb45328585cb8306c58785000e
Amenallah28 <amenallahmrabet9@gmail.com>
2024-07-19 13:59:32 +0100

    adding comments

--- haya_bismellah/forms.py
+++ haya_bismellah/forms.py
@@ -3,6 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
+# Define the form for submitting a review
+
 class ReviewForm(FlaskForm):
     rating = IntegerField('Rating (1-5)', validators=[DataRequired(), NumberRange(min=1, max=5)])
     comment = TextAreaField('Comment', validators=[DataRequired()])
