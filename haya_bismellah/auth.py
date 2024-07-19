1899a5acb6ea5398c1379124b4242af5af8a4f52
Amenallah28 <amenallahmrabet9@gmail.com>
2024-07-19 13:54:26 +0100

    adding comments

--- haya_bismellah/auth.py
+++ haya_bismellah/auth.py
@@ -4,16 +4,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+# Define a Blueprint for authentication
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    # Handle login form submission
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
+        # Retrieve user by email
         user = User.query.filter_by(email=email).first()
         if user:
+            # Check password
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
@@ -23,22 +27,26 @@ def login():
         else:
             flash('Email does not exist.', category='error')
 
+    # Render the login template
     return render_template("login.html")
 
 @auth.route('/logout')
 @login_required
 def logout():
+    # Log the user out
     logout_user()
     return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    # Handle sign-up form submission
     if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        # Validate form inputs
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
@@ -51,6 +59,7 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         else:
+            # Create a new user
             new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
@@ -58,4 +67,5 @@ def sign_up():
             flash('Account created!', category='success')
             return redirect(url_for('routes.home'))
 
+    # Render the sign-up template
     return render_template("signup.html")
